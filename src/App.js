e0e7e3345796ba4596c3d1f398c2480380b20ca4
xuzhenlu <13103615283@163.com>
2019-12-28 23:25:39 +0800

    12281225

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React from 'react';
 import './App.css';
 import Main from './pages/Main'
 import Comment from './components/Table'
+import Login from './pages/Login'
+import  {BrowserRouter as Router,Route,Redirect} from 'react-router-dom'
 function App() {
 
   return (
-    <div style={{display:'flex',justifyContent:'space-around'}}>
-      <Comment/>
-    </div>
+    <Router>
+      <Route path="/main" component={Main} exact/>
+      <Redirect from="/" to="/login" />
+      <Route path="/login" component={Login} exact />
+    </Router>
   )
 }
 
