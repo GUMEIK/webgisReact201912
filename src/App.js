fe059251cdf2f17134e732b5819081b81a59cc7b
xuzhenlu <13103615283@163.com>
2019-12-28 16:08:22 +0800

    srctest

--- src/App.js
+++ src/App.js
@@ -3,8 +3,63 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+//   var map = new window.AMap.Map('root', {
+//     center: [116.397428, 39.90923],
+//     layers: [//使用多个图层
+//         new window.AMap.TileLayer.Satellite(),
+//         new window.AMap.TileLayer.RoadNet()
+//     ],
+//     zooms: [4,18],//设置地图级别范围
+//     zoom: 13
+// });
+  // let webmap = new window.WebMap({
+  //   portalItem: {
+  //     id: "6c5d657f1cb04a5eb78a450e3c699c2a"
+  //   }
+  // });
+
+  // let view = new window.MapView({
+  //   container: "root",
+  //   map: webmap
+  // });
+
+  // view.when(function() {
+  //   view.popup.autoOpenEnabled = false; //disable popups
+
+  //   // Create the Editor
+  //   let editor = new window.Editor({
+  //     view: view
+  //   });
+
+  //   // Add widget to top-right of the view
+  //   view.ui.add(editor, "top-right");
+  // })
+  if(window.WebMap && window.MapView && window.Editor){
+    // let webmap = new window.WebMap({
+    //   portalItem: {
+    //     id: "6c5d657f1cb04a5eb78a450e3c699c2a"
+    //   }
+    // });
+  
+    // let view = new window.MapView({
+    //   container: "root",
+    //   map: webmap
+    // });
+  
+    // view.when(function() {
+    //   view.popup.autoOpenEnabled = false; //disable popups
+  
+    //   // Create the Editor
+    //   let editor = new window.Editor({
+    //     view: view
+    //   });
+  
+    //   // Add widget to top-right of the view
+    //   view.ui.add(editor, "top-right");
+    // });
+  }
   return (
-    <div className="App">
+    <div className="App" id="tttt" style={{width:500,height:500}}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
